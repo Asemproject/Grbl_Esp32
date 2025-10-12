e00de70c60e852e37cf36b006b2e179d4cba1c9b
Asem <162907069+Asemproject@users.noreply.github.com>
2025-10-12 10:44:29 +0700

    Update test_drive.h

--- Grbl_Esp32/src/Machines/test_drive.h
+++ Grbl_Esp32/src/Machines/test_drive.h
@@ -2,21 +2,11 @@
 // clang-format off
 
 /*
-    test_drive.h
+    4axis_external_driver.h
     Part of Grbl_ESP32
 
-    Pin assignments (or lack thereof) for testing Grbl_ESP32.
-
-    It creates a basic 3 axis machine without actually driving
-    I/O pins.  Grbl will report that axes are moving, but no physical
-    motor motion will occur.
-
-    This can be uploaded to an unattached ESP32 or attached to
-    unknown hardware with no risk of pins trying to output signals
-    into a short, etc that could dmamge the ESP32
-
-    It can also be used to get the basic program running so OTA
-    (over the air) firmware loading can be done.
+    Pin assignments for the buildlog.net 4-axis external driver board
+    https://github.com/bdring/4_Axis_SPI_CNC
 
     2018    - Bart Dring
     2020    - Mitch Bradley
@@ -35,21 +25,53 @@
     along with Grbl_ESP32.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define MACHINE_NAME "Test Drive - Demo Only No I/O!"
+#define MACHINE_NAME            "External 4 Axis Driver Board V2"
+
+#ifdef N_AXIS
+        #undef N_AXIS
+#endif
+#define N_AXIS 4
+
+#define X_STEP_PIN              GPIO_NUM_0
+#define X_DIRECTION_PIN         GPIO_NUM_2
+#define Y_STEP_PIN              GPIO_NUM_26
+#define Y_DIRECTION_PIN         GPIO_NUM_15
+#define Z_STEP_PIN              GPIO_NUM_27
+#define Z_DIRECTION_PIN         GPIO_NUM_33
+#define A_STEP_PIN              GPIO_NUM_12
+#define A_DIRECTION_PIN         GPIO_NUM_14
+#define STEPPERS_DISABLE_PIN    GPIO_NUM_13
+
+/*
+#define SPINDLE_TYPE            SpindleType::PWM // only one spindle at a time
+*/
+
+#define SPINDLE_OUTPUT_PIN      GPIO_NUM_25
+#define SPINDLE_ENABLE_PIN      GPIO_NUM_22
 
 
-#define N_AXIS 3
-// This cannot use homing because there are no switches
-#ifdef DEFAULT_HOMING_CYCLE_0
-    #undef DEFAULT_HOMING_CYCLE_0
+#define SPINDLE_TYPE            SpindleType::HUANYANG // only one spindle at a time
+#define VFD_RS485_TXD_PIN		GPIO_NUM_17
+#define VFD_RS485_RXD_PIN		GPIO_NUM_4
+#define VFD_RS485_RTS_PIN		GPIO_NUM_16
+
+#define X_LIMIT_PIN             GPIO_NUM_34
+#define Y_LIMIT_PIN             GPIO_NUM_35
+#define Z_LIMIT_PIN             GPIO_NUM_36
+
+#if (N_AXIS != 3)
+        #define A_LIMIT_PIN     GPIO_NUM_39
 #endif
 
-#ifdef DEFAULT_HOMING_CYCLE_1
-    #undef DEFAULT_HOMING_CYCLE_1
-#endif
+#define PROBE_PIN               GPIO_NUM_32
+#define COOLANT_MIST_PIN        GPIO_NUM_21
 
-#define SPINDLE_TYPE    SpindleType::NONE
+// ===================== SD CARD (SPI MODE) =====================
+// Kamu bisa sesuaikan pin berikut sesuai dengan wiring board kamu
+#define SD_CARD_MOSI_PIN        GPIO_NUM_23
+#define SD_CARD_MISO_PIN        GPIO_NUM_19
+#define SD_CARD_SCK_PIN         GPIO_NUM_18
+#define SD_CARD_CS_PIN          GPIO_NUM_5
 
-#ifdef USE_RMT_STEPS
-#undef USE_RMT_STEPS  // Suppress unused variable warning
-#endif
+// Aktifkan dukungan SD card di Grbl_ESP32
+#define USE_SD_CARD
